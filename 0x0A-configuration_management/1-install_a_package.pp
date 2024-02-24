03e64e39f9e5120c0e59a8c6c51747af33fd652b
Solomon Chidera Oyoke <116091273+SolomonChidera@users.noreply.github.com>
2024-02-24 23:47:27 +0100

    Update 1-install_a_package.pp
    Install flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,21 @@
-# Using Puppet, installing flask from pip3
+# play with flask
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  name     => 'flask'
+}
+
+package { 'python3-pip':
+  ensure   => installed,
+
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
 }
